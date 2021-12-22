ff902b6169e39a457e037bba82da4078870eb26c
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-12-22 19:56:36 +0530

    Stack Part 01

---
+++ stack_in_java/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack nums = new Stack();
+        nums.push(2);
+        nums.push(5);
+        nums.push(6);
+        nums.push(10);
+        nums.show();
+        nums.pop();
+        nums.show();
+        nums.peek();
+
+    }
+}
---
+++ stack_in_java/src/com/company/Stack.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Stack {
+
+    int[] stack = new int[5];
+    int top = 0;
+
+    public void push(int data){
+        stack[top] = data;
+        top++;
+    }
+
+    public void show(){
+        System.out.print("Printing array --> ");
+        for(int i=0; i<5; i++){
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void pop(){
+        top--;
+        int data = stack[top];
+        System.out.println("Removed " + data + " from stack.");
+        stack[top] = 0;
+
+    }
+
+    public void peek(){
+        System.out.println("Peek Value " + stack[top-1]);
+    }
+}
